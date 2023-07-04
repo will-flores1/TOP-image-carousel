3ee7a1872380d590fa6a2fe0f54cee94536e82a4
wilfredo-flores <willflores1221@gmail.com>
2023-07-03 23:28:53 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+let slides = document.getElementsByClassName("slider__slide");
+let navDots = Array.from(document.getElementsByClassName("slider__navlink"));
+
+let nextButton = document.getElementById("nav-button--next");
+let prevButton = document.getElementById("nav-button--prev");
+
+let currentSlide = 0;
+let nextSlide = 0;
+
+function parseNextSlide() {
+	nextSlide = (nextSlide + slides.length) % slides.length;
+}
+
+function toggleClasses(className, ...elementArr) {
+	if (elementArr.length === 0) return;
+
+	elementArr.forEach((element) => {
+		element.classList.toggle(className);
+	});
+}
+function changeSlide(newSlide) {
+	nextSlide = newSlide;
+	parseNextSlide();
+	toggleClasses(
+		"active",
+		slides[currentSlide],
+		navDots[currentSlide],
+		slides[nextSlide],
+		navDots[nextSlide]
+	);
+
+	currentSlide = nextSlide;
+}
+
+nextButton.addEventListener("click", () => {
+	changeSlide(currentSlide + 1);
+});
+
+prevButton.addEventListener("click", () => {
+	changeSlide(currentSlide - 1);
+});
+
+navDots.forEach((bullet, bulletIndex) => {
+	bullet.addEventListener("click", () => {
+		console.log(currentSlide, bulletIndex);
+		if (currentSlide !== bulletIndex) {
+			changeSlide(bulletIndex);
+		}
+	});
+});
